0dd3e468407f15136f8394cf591f3681fcda7d6e
szboynono <szboylbd@hotmail.com>
2021-02-23 01:50:37 -0500

    docs: update styling of the demo

--- docs/.vitepress/theme/components/ButtonDemo.vue
+++ docs/.vitepress/theme/components/ButtonDemo.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="button-demo">
     <template v-if="type==='basic'">
-      <m-button type="primary">Primary</m-button>
-      <m-button type="success">Success</m-button>
-      <m-button type="warning">Warning</m-button>
-      <m-button type="danger">Danger</m-button>
+      <m-button class="button-demo-item" type="primary">Primary</m-button>
+      <m-button class="button-demo-item" type="success">Success</m-button>
+      <m-button class="button-demo-item" type="warning">Warning</m-button>
+      <m-button class="button-demo-item" type="danger">Danger</m-button>
     </template>
     <template v-else-if="type==='outline'">
-      <m-button type="primary" outline>Primary</m-button>
-      <m-button type="success" outline>Success</m-button>
-      <m-button type="warning" outline>Warning</m-button>
-      <m-button type="danger" outline>Danger</m-button>
+      <m-button class="button-demo-item" type="primary" outline>Primary</m-button>
+      <m-button class="button-demo-item" type="success" outline>Success</m-button>
+      <m-button class="button-demo-item" type="warning" outline>Warning</m-button>
+      <m-button class="button-demo-item" type="danger" outline>Danger</m-button>
     </template>
     <template v-else-if="type==='size'">
-      <m-button type="primary" size="lg">Large</m-button>
-      <m-button type="primary" size="sm">Small</m-button>
+      <m-button class="button-demo-item" type="primary" size="lg">Large</m-button>
+      <m-button class="button-demo-item" type="primary" size="sm">Small</m-button>
     </template>
     <template v-else-if="type==='disabled'">
-      <m-button type="primary" disabled>Primary(disabled)</m-button>
-      <m-button type="primary" outline disabled>Primary(outline & disabled)</m-button>
+      <m-button class="button-demo-item" type="primary" disabled>Primary(disabled)</m-button>
+      <m-button class="button-demo-item" type="primary" outline disabled>Primary(outline & disabled)</m-button>
     </template>
   </div>
 </template>
@@ -38,7 +38,8 @@ export default {
 <style lang="scss" scoped>
 .button-demo {
   margin-top: 16px;
-  button {
+  
+  & .button-demo-item {
     margin: 4px;
   }
 }
