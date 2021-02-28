ee6574c5bb2c52823092ca5f0cd339c852188cf2
szboynono <szboylbd@hotmail.com>
2021-02-27 20:35:08 -0500

    feat: update docs

--- docs/.vitepress/theme/components/MessageDemo.vue
+++ docs/.vitepress/theme/components/MessageDemo.vue
@@ -1,44 +1,142 @@
 <template>
-  <m-button @click="showMsgDefault" type="primary">Default</m-button>
-  <m-button @click="showMsgInfo" type="primary">Info</m-button>
-  <m-button @click="showMsgSuccess" type="success">Success</m-button>
-  <m-button @click="showMsgWarning" type="warning">Warning</m-button>
-  <m-button @click="showMsgDanger" type="danger">Danger</m-button>
+  <div class="message-demo">
+    <template v-if="mode === 'basic'">
+      <m-button
+        class="message-demo__item"
+        @click="showMsgDefault"
+        type="primary"
+        >Default</m-button
+      >
+      <m-button class="message-demo__item" @click="showMsgInfo" type="primary"
+        >Info</m-button
+      >
+      <m-button
+        class="message-demo__item"
+        @click="showMsgSuccess"
+        type="success"
+        >Success</m-button
+      >
+      <m-button
+        class="message-demo__item"
+        @click="showMsgWarning"
+        type="warning"
+        >Warning</m-button
+      >
+      <m-button class="message-demo__item" @click="showMsgDanger" type="danger"
+        >Danger</m-button
+      >
+    </template>
+    <template v-else-if="mode === 'closable'">
+      <m-button
+        class="message-demo__item"
+        @click="showMsgDefault"
+        type="primary"
+        >Default</m-button
+      >
+      <m-button class="message-demo__item" @click="showMsgInfo" type="primary"
+        >Info</m-button
+      >
+      <m-button
+        class="message-demo__item"
+        @click="showMsgSuccess"
+        type="success"
+        >Success</m-button
+      >
+      <m-button
+        class="message-demo__item"
+        @click="showMsgWarning"
+        type="warning"
+        >Warning</m-button
+      >
+      <m-button class="message-demo__item" @click="showMsgDanger" type="danger"
+        >Danger</m-button
+      >
+    </template>
+    <template v-else-if="mode === 'timeout'">
+      <m-button
+        class="message-demo__item"
+        @click="showMsgTimeout"
+        type="primary"
+        >10s timeout</m-button
+      >
+    </template>
+    <template v-else-if="mode === 'onClose'">
+      <m-button
+        class="message-demo__item"
+        @click="showMsgOnClose"
+        type="primary"
+        >Alert after message</m-button
+      >
+    </template>
+  </div>
 </template>
 
 <script>
 import { onMounted } from 'vue'
 export default {
   name: 'MessageDemo',
+  props: {
+    mode: String,
+  },
   methods: {
     showMsgDefault() {
-      this.$MMessage(
-        {
-          message: 'This is some dummy message here',
-          onClose: () => {
-            console.log('closed')
-          } 
-        }
-      )
+      this.$MMessage({
+        message: 'This is some dummy message here',
+        closable: this.mode === 'closable',
+      })
     },
     showMsgInfo() {
-      this.$MMessage({message: 'This is some dummy message here', type: 'info'})
+      this.$MMessage({
+        message: 'This is some dummy message here',
+        type: 'info',
+        closable: this.mode === 'closable',
+      })
     },
     showMsgSuccess() {
-      this.$MMessage({message: 'This is some dummy message here', type: 'success'})
+      this.$MMessage({
+        message: 'This is some dummy message here',
+        type: 'success',
+        closable: this.mode === 'closable',
+      })
     },
     showMsgWarning() {
-      this.$MMessage({message: 'This is some dummy message here', type: 'warning'})
+      this.$MMessage({
+        message: 'This is some dummy message here',
+        type: 'warning',
+        closable: this.mode === 'closable',
+      })
     },
     showMsgDanger() {
-      this.$MMessage({message: 'This is some dummy message here', type: 'danger'})
+      this.$MMessage({
+        message: 'This is some dummy message here',
+        type: 'danger',
+        closable: this.mode === 'closable',
+      })
     },
+    showMsgTimeout() {
+      this.$MMessage({
+        message: 'This is some dummy message here',
+        timeout: 10000
+      })
+    },
+    showMsgOnClose() {
+      this.$MMessage({
+        message: 'This is some dummy message here',
+        onClose: () => {
+          alert('Hi There!')
+        }
+      })
+    }
   },
 }
 </script>
 
-<style scoped>
-div {
-  margin-top: 8px;
+<style lang="scss">
+.message-demo {
+  margin-top: 16px;
+
+  &__item {
+    margin: 4px;
+  }
 }
 </style>
--- docs/components/message.md
+++ docs/components/message.md
@@ -1,3 +1,128 @@
 # Message
 
-<MessageDemo />
+Just a message
+
+## Basic usage
+
+Mosha exposes a global function `$MMessage({...options})` for the message.
+The message will slide in from the top and disappear after 5 seconds once called.
+
+<MessageDemo mode="basic" />
+
+```ts
+<template>
+  <m-button @click="showMsgDefault" type="primary">Default</m-button>
+  <m-button @click="showMsgInfo" type="primary">Info</m-button>
+  <m-button @click="showMsgSuccess" type="success">Success</m-button>
+  <m-button @click="showMsgWarning" type="warning">Warning</m-button>
+  <m-button @click="showMsgDanger" type="danger">Danger</m-button>
+</template>
+
+<script>
+import { onMounted } from 'vue'
+export default {
+  name: 'demo',
+  methods: {
+    showMsgDefault() {
+      this.$MMessage({message: 'This is some dummy message here',})
+    },
+    showMsgInfo() {
+      this.$MMessage({message: 'This is some dummy message here', type: 'info'})
+    },
+    showMsgSuccess() {
+      this.$MMessage({message: 'This is some dummy message here', type: 'success'})
+    },
+    showMsgWarning() {
+      this.$MMessage({message: 'This is some dummy message here', type: 'warning'})
+    },
+    showMsgDanger() {
+      this.$MMessage({message: 'This is some dummy message here', type: 'danger'})
+    },
+  },
+}
+```
+
+## Closable
+
+Give the user the ablity to close the message
+
+<MessageDemo mode="closable" />
+
+```ts
+<template>
+  <m-button @click="showMsgDefault" type="primary">Default</m-button>
+  <m-button @click="showMsgInfo" type="primary">Info</m-button>
+  <m-button @click="showMsgSuccess" type="success">Success</m-button>
+  <m-button @click="showMsgWarning" type="warning">Warning</m-button>
+  <m-button @click="showMsgDanger" type="danger">Danger</m-button>
+</template>
+
+<script>
+import { onMounted } from 'vue'
+export default {
+  name: 'demo',
+  methods: {
+    showMsgDefault() {
+      this.$MMessage({message: 'This is some dummy message here', closable: true})
+    },
+    showMsgInfo() {
+      this.$MMessage({message: 'This is some dummy message here', type: 'info', closable: true})
+    },
+    showMsgSuccess() {
+      this.$MMessage({message: 'This is some dummy message here', type: 'success', closable: true})
+    },
+    showMsgWarning() {
+      this.$MMessage({message: 'This is some dummy message here', type: 'warning', closable: true})
+    },
+    showMsgDanger() {
+      this.$MMessage({message: 'This is some dummy message here', type: 'danger', closable: true})
+    },
+  },
+}
+```
+
+## Timeout
+
+Override the default timeout by specify an `timeout: <time in ms: number>` property in the options 
+
+<MessageDemo mode="timeout" />
+
+```ts
+<template>
+  <m-button @click="showMsgTimeout" type="primary">10s timeout</m-button>
+</template>
+
+<script>
+import { onMounted } from 'vue'
+export default {
+  name: 'demo',
+  methods: {
+    showMsgTimeout() {
+      this.$MMessage({message: 'This is some dummy message here', timeout: 10000})
+    },
+  },
+}
+```
+
+## onClose handler
+
+You can pass a function to have it called right after the message disappears
+
+<MessageDemo mode="onClose"/>
+
+```ts
+<template>
+  <m-button @click="showMsgOnClose" type="primary">Alert after message</m-button>
+</template>
+
+<script>
+import { onMounted } from 'vue'
+export default {
+  name: 'demo',
+  methods: {
+    showMsgOnClose() {
+      this.$MMessage({message: 'This is some dummy message here', onClose: () => { alert('Hi There!') }})
+    },
+  },
+}
+```
